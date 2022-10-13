63c84792640fecb62565167e9a2dbf39563fd676
VaultDweller13 <45403328+VaultDweller13@users.noreply.github.com>
2022-10-13 15:14:04 +0400

    feat: file-names task done

--- src/file-names.js
+++ src/file-names.js
@@ -15,11 +15,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  let k = 1;
+  const duplicates = {};
+  const result = [];
+  
+  for (let i = 0; i < names.length; i++) {
+    const currentFile = names[i];
+
+    if (duplicates[currentFile]) {
+      const newFile = `${currentFile}(${duplicates[currentFile]})`;
+      result.push(newFile);
+      duplicates[currentFile]++;
+      duplicates[newFile] = 1;
+    } else {
+      duplicates[currentFile] = 1;
+      result.push(currentFile);
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
   renameFiles
 };
+
+console.log(renameFiles([]));
+
+// assert.deepEqual(renameFiles(['doc', 'doc', 'image', 'doc(1)', 'doc']), ['doc', 'doc(1)', 'image', 'doc(1)(1)', 'doc(2)']);
+//     assert.deepEqual(renameFiles(['a', 'b', 'cd', 'b ', 'a(3)']), ['a', 'b', 'cd', 'b ', 'a(3)']);
+//     assert.deepEqual(renameFiles([]), []);
